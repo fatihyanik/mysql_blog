22f862ade7861a6d4482d1ed89184986d98f76f0
fatihyanik <fatihyanik07@gmail.com>
2022-09-27 15:42:54 +0200

    added login and regster page design

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Register() {
   return (
-    <div>Register</div>
-  )
+    <div className="auth">
+      <h1>Register</h1>
+      <form>
+        <input type="text" placeholder="username" required></input>
+        <input type="email" placeholder="email" required></input>
+        <input type="password" placeholder="password" required></input>
+        <button>Register</button>
+        <p>This is an error!</p>
+        <span>
+          Do you have an account?
+          <Link to="/login">Login</Link>
+        </span>
+      </form>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
